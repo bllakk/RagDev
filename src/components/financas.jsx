af7c3c35c0394ad734e33b9ae1283f37f636870c
Gyan Monteiro <102252895+bjmontr@users.noreply.github.com>
2024-09-27 16:52:23 -0300

    .

--- src/components/financas.jsx
+++ src/components/financas.jsx
@@ -1,35 +1,48 @@
-import InputDefault from "./inputs";
-import BtnCustom from "./btn-custom";
-export default function Financas(){
-    return(
+import InputDefault from './inputs';
+import BtnCustom from './btn-custom';
+export default function Financas() {
+    return (
         <div>
-            <div className="flex justify-center gap-6">
-                <div>
-                    <h1 className="font-impact tracking-widest text-base  text-slate-300">LUCRO DO MES / ZENY</h1>
-                    <h2 className="font-impact tracking-widest text-xl text-slate-300">Z$</h2>
+            <div className="flex flex-wrap justify-center gap-6">
+                <div className="w-full sm:w-auto text-center">
+                    <h1 className="font-impact tracking-widest text-base text-slate-300">
+                        LUCRO DO MÊS / ZENY
+                    </h1>
+                    <h2 className="font-impact tracking-widest text-xl text-slate-300">
+                        Z$
+                    </h2>
                 </div>
-                <div>
-                    <h1 className="font-impact tracking-widest text-base  text-slate-300">LUCRO DO MES / REAL</h1>
-                    <h2 className="font-impact tracking-widest text-xl text-slate-300">R$</h2>
+
+                <div className="w-full sm:w-auto text-center">
+                    <h1 className="font-impact tracking-widest text-base text-slate-300">
+                        LUCRO DO MÊS / REAL
+                    </h1>
+                    <h2 className="font-impact tracking-widest text-xl text-slate-300">
+                        R$
+                    </h2>
                 </div>
             </div>
-            <div className="flex gap-5 justify-center mt-5">
-            <div>
-                <h1>ADICIONAR GANHOS DIARIO</h1>
-                <div className="flex justify-center items-center text-center gap-5">
-                    <InputDefault text={"Z$"}/>
-                    <BtnCustom text={"ADICIONAR VALOR"}/>
+
+            <div className="flex flex-wrap gap-5 justify-center mt-5">
+                <div className="w-full sm:w-auto text-center">
+                    <h1 className="font-semibold text-slate-300">
+                        ADICIONAR GANHOS DIÁRIO
+                    </h1>
+                    <div className="flex flex-col sm:flex-row justify-center items-center gap-5">
+                        <InputDefault text={'Z$'} />
+                        <BtnCustom text={'ADICIONAR VALOR'} />
+                    </div>
                 </div>
-            </div>
-            <div>
-                <h1>GASTOS DESSE MES</h1>
-                <div className="flex justify-center items-center text-center gap-5">
-                    <InputDefault text={"INVESTIMENTO"}/>
-                    <InputDefault text={"Z$"}/>
-                    <BtnCustom text={"ADICIONAR VALOR"}/>
+
+                <div className="w-full sm:w-auto text-center">
+                    <h1 className="font-semibold text-slate-300">GASTOS DESSE MÊS</h1>
+                    <div className="flex flex-col sm:flex-row justify-center items-center gap-5">
+                        <InputDefault text={'INVESTIMENTO'} />
+                        <InputDefault text={'Z$'} />
+                        <BtnCustom text={'ADICIONAR VALOR'} />
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     );
-} 
+}
