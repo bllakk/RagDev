62dbda2728df6ce687b740b8f3b692d632c76ea5
Monteiro <102252895+bllakk@users.noreply.github.com>
2024-10-14 15:29:27 -0300

    Delete client directory

--- client/src/components/alert.jsx
+++
@@ -1,9 +0,0 @@
-export default function Alert({ errorMessage }) {
-    return (
-        <>
-            {errorMessage && <p className="text-purple-500 font-poppins">
-                {errorMessage}
-                </p>}
-        </>
-    );
-}
