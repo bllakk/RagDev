1788fe4bce327dccd096dc47cf1bc44476edbacc
Gyan Monteiro <102252895+bjmontr@users.noreply.github.com>
2024-10-15 11:52:42 -0300

    token

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,54 +6,42 @@ import Calculadora from '../components/calculadora';
 import Financas from '../components/financas';
 
 export default function Home() {
-
-
   const navigate = useNavigate();
 
   useEffect(() => {
     const verifyToken = async () => {
-      const token = localStorage.getItem('authToken'); // Pegando o token do localStorage
-      if (!token) {
-        // Se o token não existir, redireciona imediatamente para a página de login
-        navigate('/RagDev');
-        return;
-      }
-
       try {
         const response = await axios.get('https://server-rag.vercel.app/home', {
-          headers: {
-            Authorization: `Bearer ${token}`
-          },
-          withCredentials: true
+          withCredentials: true // Garantindo que os cookies sejam enviados com a requisição
         });
 
         if (response.status === 200 && response.data.message) {
+          console.log(response.data.message); // Acesso bem-sucedido
         } else {
           throw new Error('Token inválido');
         }
       } catch (error) {
-        localStorage.removeItem('authToken');
-        navigate('/RagDev');
+        localStorage.removeItem('authToken'); // Remover o token do localStorage se necessário
+        navigate('/RagDev'); // Redirecionar para a página de login
       }
     };
 
     verifyToken();
   }, [navigate]);
 
-  const [activeComponent, setActiveComponent] = useState('calculadora'); // Estado para controlar o componente ativo
+  const [activeComponent, setActiveComponent] = useState('calculadora');
 
   const handleButtonClick = (component) => {
     setActiveComponent(component);
   };
 
-
   return (
     <div className='bg-dark-blue-0 h-screen overflow-x-hidden'>
       <Nav onButtonClick={handleButtonClick} />
       <div>
-      {activeComponent === 'calculadora' && <Calculadora />}
-      {activeComponent === 'financas' && <Financas />}
-        </div>
+        {activeComponent === 'calculadora' && <Calculadora />}
+        {activeComponent === 'financas' && <Financas />}
+      </div>
     </div>
   );
 }
